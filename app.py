40f59c6f3a643362b971a1f3979a2de7b78d0b53
Dermot Murphy <dermotmurphy@Dermots-MacBook-Pro.local>
2020-11-16 01:30:31 +0000

    added app.py main function and env variables for connection to db

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, render_template, flash, redirect, request, session, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+
+if os.path.exists("env.py"):
+   import env
+
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = os.environ.get("MONGODB_NAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+@app.route("/get_reviews")
+def get_tasks():
+    reviews = list(mongo.db.reviews.find())
+    return render_template("reviews.html")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
